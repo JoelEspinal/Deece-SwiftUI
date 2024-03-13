d7d8bb614d8bf58554864bcd4120dd11712fc466
JoelEspinal <joel_espinal@claro.com.do>
2024-03-13 13:58:08 -0400

    Add project assets

---
+++ Deece SwiftUI/Deece SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Deece SwiftUI
+//
+//  Created by Joel Espinal (ClaroDom) on 13/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Deece SwiftUI/Deece SwiftUI/Deece_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Deece_SwiftUIApp.swift
+//  Deece SwiftUI
+//
+//  Created by Joel Espinal (ClaroDom) on 13/3/24.
+//
+
+import SwiftUI
+
+@main
+struct Deece_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
